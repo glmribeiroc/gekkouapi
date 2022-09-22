c48def66c268867c94849592e7508aff0537828f
Guilherme <glmribeiroc@gmail.com>
2022-09-22 15:15:27 -0300

    feat: create refresh token path and secure to the paths

--- src/main/java/br/com/gekkou/gekkouapi/modules/account/controller/AccountController.java
+++ src/main/java/br/com/gekkou/gekkouapi/modules/account/controller/AccountController.java
@@ -1,13 +1,32 @@
 package br.com.gekkou.gekkouapi.modules.account.controller;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import br.com.gekkou.gekkouapi.modules.account.controller.request.AddRoleToUserRequest;
 import br.com.gekkou.gekkouapi.modules.account.entity.Account;
 import br.com.gekkou.gekkouapi.modules.account.entity.Role;
@@ -17,30 +36,73 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/api/account")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class AccountController {
     private final AccountService accountService;
 
-    @GetMapping()
+    @GetMapping("/account")
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<List<Account>> getUsers() {
         return ResponseEntity.ok().body(accountService.getAccounts());
     }
 
-    @PostMapping()
+    @PostMapping("/account/create")
     public ResponseEntity<Account> createAccount(@RequestBody Account account) {
         return ResponseEntity.status(HttpStatus.CREATED).body(accountService.createAccount(account));
     }
 
     @PostMapping("/role/create")
-    public ResponseEntity<Role> createAccount(@RequestBody Role role) {
+    @Secured("ROLE_ADMIN")
+    public ResponseEntity<Role> createRole(@RequestBody Role role) {
         return ResponseEntity.status(HttpStatus.CREATED).body(accountService.createRole(role));
     }
 
-    @PostMapping("/addrole")
+    @PostMapping("/role/addtouser")
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<?> addRoleToUser(@RequestBody AddRoleToUserRequest request) {
         accountService.addRoleToUser(request.email(), request.roleName());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response)
+            throws StreamWriteException, DatabindException, IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String refresh_token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("1e79c331efeb01e5db9cc16cb7e088dd".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refresh_token);
+                String username = decodedJWT.getSubject();
+                Account account = accountService.getAccount(username);
+                String access_token = JWT.create()
+                        .withSubject(account.getEmail())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 1 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles",
+                                account.getRoles().stream().map(Role::getName)
+                                        .collect(Collectors.toList()))
+                        .sign(algorithm);
+
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", access_token);
+                tokens.put("refresh_token", refresh_token);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+            } catch (Exception e) {
+                response.setHeader("error", e.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                Map<String, String> error = new HashMap<>();
+                error.put("error_message", e.getMessage());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+
+        } else {
+            throw new RuntimeException("Refresh token is missing!");
+        }
     }
 
 }
