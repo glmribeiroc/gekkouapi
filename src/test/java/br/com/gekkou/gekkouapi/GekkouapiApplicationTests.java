bec8f141416b924bbbc12cdfe0ead43c08949507
Guilherme <glmribeiroc@gmail.com>
2022-09-22 02:29:10 -0300

    initial project

---
+++ src/test/java/br/com/gekkou/gekkouapi/GekkouapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gekkou.gekkouapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GekkouapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
