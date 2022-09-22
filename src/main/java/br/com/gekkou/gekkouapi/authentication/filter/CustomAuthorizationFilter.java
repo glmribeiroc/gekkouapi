81c6e7b954174e4febe0274fd82f36b5d44fe600
Guilherme <glmribeiroc@gmail.com>
2022-09-22 15:13:35 -0300

    refactor: change the login path

--- src/main/java/br/com/gekkou/gekkouapi/authentication/filter/CustomAuthorizationFilter.java
+++ src/main/java/br/com/gekkou/gekkouapi/authentication/filter/CustomAuthorizationFilter.java
@@ -35,7 +35,8 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/login")) {
+        if (request.getServletPath().equals("/api/auth/session")
+                || request.getServletPath().equals("/api/token/refresh")) {
             filterChain.doFilter(request, response);
         } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
